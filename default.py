53a5052dc150fce67339ef249512e8ae7782ab72
harryberlin <avr-ibus@gmx.net>
2022-09-08 22:49:48 +0200

    github repo release

---
+++ default.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+
+__author__ = 'devkid/stanley87/harryberlin'
+
+import os
+import sys
+import json
+import time
+import xbmc
+import xbmcgui
+import xbmcaddon
+import xbmcvfs
+import datetime
+
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    #del unicode_literals
+    from xbmc import translatePath as xbmc_translate_path
+else:
+    from xbmcvfs import translatePath as xbmc_translate_path
+
+ENCODING = sys.getdefaultencoding()
+
+ADDON = xbmcaddon.Addon('script.reliableresume')
+ADDON_NAME = ADDON.getAddonInfo('name')
+ADDON_ID = ADDON.getAddonInfo('id')
+ADDON_VERSION = ADDON.getAddonInfo('version')
+ADDON_PATH = ADDON.getAddonInfo('path')
+ADDON_USER_PATH = os.path.join(xbmc_translate_path('special://userdata'), 'addon_data', ADDON_ID)
+ADDON_ICON = os.path.join(ADDON_PATH, 'icon.png')
+
+DATAFILE = os.path.join(ADDON_USER_PATH, 'ResumeSaverA.m3u')
+DATAFILE2 = os.path.join(ADDON_USER_PATH, 'ResumeSaverB.m3u')
+
+if PY2:
+    XBMC_LOG_LEVEL = xbmc.LOGNOTICE
+else:
+    XBMC_LOG_LEVEL = xbmc.LOGINFO
+
+
+def get_addon_setting(id):
+    setting = xbmcaddon.Addon(ADDON_ID).getSetting(id)
+    if setting.lower() == 'true': return True
+    if setting.lower() == 'false': return False
+    return str(setting)
+
+
+class ResumePlayer:
+    rewind_before_play = {'0': 0.0, '1': 5.0, '2': 15.0, '3': 60.0, '4': 180.0, '5': 300.0}
+
+    rewind_s = rewind_before_play[get_addon_setting('rewind_before_play')]
+
+    def main(self):
+        if os.path.exists(DATAFILE) or os.path.exists(DATAFILE2):
+            if not self.opendata():
+                return note("""Can't resume corrupt file""")
+        else:
+            return note('''Can't resume''', 'No File exist')
+
+        if self.plsize == False:  # there is no playlist
+            xbmc.Player().play(self.playing)
+            self.seekTime(self.time)
+
+        if get_addon_setting('volume') and self.volume is not False:
+            volume_set(self.volume)
+
+        if self.media == 'audio':
+            self.plist = xbmc.PlayList(0)
+        elif self.media == 'video':
+            self.plist = xbmc.PlayList(1)
+        else:
+            self.plist = xbmc.PlayList(0)
+
+        self.plist.clear()
+
+        self.plist.load(self.datafile)
+        # log(self.plist.size())
+        if self.plsize < 1:
+            return
+
+
+        if self.playing:
+            xbmc.Player().play(item=self.plist, windowed=False, startpos=self.plpos)
+
+
+        self.can_play = False
+        from threading import Thread
+        self.can_play_thread = Thread(target=self.player_has_started)
+        self.can_play_thread.daemon = True
+        self.can_play_thread.start()
+
+        self.can_play_thread.join()
+
+        if not self.can_play:
+            xbmc.Player().stop()
+            return note('''Can't resume''', 'File from Playlist is not available')
+
+        if self.playing.find("pvr://") > -1:
+            debug('Skip Seeking, because Video is PVR (Live TV)')
+        else:
+            self.seekTime(self.time)
+
+        if get_addon_setting('pause_on_startup') and xbmc.Player().isPlaying():
+            xbmc.Player().pause()
+
+    def seekTime(self, seekTo):
+        xbmc.sleep(1000)  # wait 'a bit'. if doing seek directly it does not work when we just started playing
+        if xbmc.Player().isPlaying(): 
+            xbmc.Player().seekTime(seekTo)
+
+    def player_has_started(self):
+        for counter in range(5000, 0, -1):
+            if xbmc.Player().isPlaying():
+                if xbmc.Player().getTime() > 0.0:
+                    self.can_play = True
+                    return
+            xbmc.sleep(1)
+        self.can_play = False
+
+    def opendata(self):
+        firstFile = DATAFILE
+        secondFile = DATAFILE2
+
+        if (os.access(firstFile, os.F_OK) and os.access(secondFile, os.F_OK)):
+            log('Both files existing. checking which is newer')
+            if (os.path.getctime(secondFile) > os.path.getctime(firstFile)):
+                firstFile = DATAFILE2
+                secondFile = DATAFILE
+                log('swapping files')
+
+        try:
+            return self.opendataex(firstFile)
+        except:
+            return self.opendataex(secondFile)
+
+    def opendataex(self, datafile):
+        self.playlist = []
+        self.datafile = datafile
+        log(self.datafile)
+        
+        with open(datafile, mode='rb') as fh:
+            fh_str = fh.read()
+            if not PY2:
+                fh_str = fh_str.decode('utf-8')
+                
+            if fh_str.find('#STORE::DONE') < 0:
+                return False
+
+            self.volume = False
+            for line in fh_str.splitlines():
+                debug('line: [%s]' % line)
+                theLine = line.strip()
+                if theLine.startswith('#WINDOW::'):
+                    self.window = theLine[9:]
+                if theLine.startswith('#VOLUME::'):
+                    self.volume = theLine[9:]
+                if theLine.startswith('#TIME::'):
+                    self.time = theLine[7:]
+                    if self.time == '-':
+                        self.time = False
+                    else:
+                        self.time = float(self.time)
+                    self.time = max(0.0, self.time - self.rewind_s)
+                if theLine.startswith('#PLPOS::'):
+                    self.plpos = theLine[8:]
+                    if self.plpos == "-":
+                        self.plpos = False
+                    else:
+                        self.plpos = int(self.plpos)
+                if theLine.startswith('#PLSIZE::'):
+                    self.plsize = theLine[9:]
+                    if self.plsize == '-':
+                        self.plsize = False
+                    else:
+                        self.plsize = int(self.plsize)
+                if theLine.startswith('#PLAYING::'):
+                    self.playing = theLine[10:]
+                    if self.playing == '-':
+                        self.playing = False
+                if theLine.startswith('#MEDIA::'):
+                    self.media = theLine[8:]
+                    if self.media == '-':
+                        self.media = False
+        
+        return True
+
+    def checkme(self):
+        self.plist = xbmc.PlayList(0)
+        self.plsize = self.plist.size()
+        if self.plsize != 0:
+            self.media = 'audio'
+            for i in range(0, self.plsize):
+                temp = self.plist[i]
+                self.playlist.append(xbmc.PlayListItem.getfilename(temp))
+            return
+        else:
+            pass
+        self.plist = xbmc.PlayList(1)
+        self.plsize = self.plist.size()
+        if self.plsize != 0:
+            self.media = 'video'
+            for i in range(0, self.plsize):
+                temp = self.plist[i]
+                self.playlist.append(xbmc.PlayListItem.getfilename(temp))
+            return
+        else:
+            self.media = '-'
+            self.plsize = '-'
+            self.playlist = '-'
+            return
+
+
+def volume_set(volume):
+    # xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Application.SetVolume", "params": {"volume": %s}, "id": 1}' % volume)
+    xbmc.executebuiltin('SetVolume(%s, False)' % volume)
+    debug('VOLUME: Set = %s' % volume)
+
+
+def open_settings():
+    # log('Window: %s' % xbmcgui.getCurrentWindowId())
+    # log('Dialog: %s' % xbmcgui.getCurrentWindowDialogId())
+    if xbmcgui.getCurrentWindowDialogId() == 10140:
+        xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Input.Back", "id": 1 }')
+    else:
+        xbmcaddon.Addon().openSettings()
+
+
+def log(msg):
+    if not PY2:
+        msg = msg.encode("utf-8", "surrogateescape").decode("ISO-8859-1")
+    xbmc.log('%s: LDR: %s' % (ADDON_ID, msg), XBMC_LOG_LEVEL)
+
+
+def note(heading, message=None, time=-1, icon=None):
+    if time == -1:
+        xbmcgui.Dialog().notification(heading='%s' % heading, message='%s' % message if message else ' ', icon='%s' % (icon if icon else ADDON_ICON))
+    else:
+        xbmcgui.Dialog().notification(heading='%s' % heading, message='%s' % message if message else ' ', icon='%s' % (icon if icon else ADDON_ICON), time=time)
+    log('NOTIFICATION: "%s%s"' % (heading, ' - %s' % message if message else ''))
+
+
+def dialog_yesno(label1, label2=None, label3=None, nolabel='', yeslabel='', autoclose=0):
+    if PY2:
+        return xbmcgui.Dialog().yesno(ADDON_NAME, line1=label1, line2=label2, line3=label3, nolabel=nolabel, yeslabel=yeslabel, autoclose=autoclose)
+    else:
+        return xbmcgui.Dialog().yesno(ADDON_NAME, message='%s%s%s' % (label1, ' - %s' % label2 if label2 else '', ' - %s' % label3 if label3 else ''), nolabel='Cancel', yeslabel='Delete', autoclose=autoclose)
+
+
+def debug(string):
+    if not get_addon_setting('debug'):
+        return
+    log('DEBUG: %s' % string)
+
+
+def delete_m3u():
+    if not dialog_yesno('Sure to delete M3u Files?'):
+        return
+    file_a_deleted = False
+    file_b_deleted = False
+    try:
+        os.remove(DATAFILE)
+        file_a_deleted = True
+    except:
+        pass
+    try:
+        os.remove(DATAFILE2)
+        file_b_deleted = True
+    except:
+        pass
+    note('M3U Files deleted!', '%s %s' % ('ResumeSaverA.m3u' if file_a_deleted else '', 'ResumeSaverB.m3u' if file_b_deleted else ''))
+
+
+def main():
+    count = len(sys.argv) - 1
+    if count > 0:
+        log(sys.argv[1])
+        given_args = sys.argv[1].split(';')
+        if str(given_args[0]) == "delete_m3u":
+            delete_m3u()
+        #elif str(given_args[0]) == "settings":
+        #    open_settings()
+        #elif str(given_args[0]) == 'update':
+        #    update()
+        else:
+            note('Unknown Arguments given!', '%s' % given_args)
+    else:
+        m = ResumePlayer()
+        m.main()
+        del m
+
+
+if __name__ == '__main__':
+    main()
---
+++ observer.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+
+__author__ = 'devkid/stanley87/harryberlin'
+
+import os
+import sys
+import json
+import time
+import xbmc
+import xbmcgui
+import xbmcaddon
+import xbmcvfs
+import datetime
+
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    #del unicode_literals
+    from xbmc import translatePath as xbmc_translate_path
+else:
+    from xbmcvfs import translatePath as xbmc_translate_path
+
+ENCODING = sys.getdefaultencoding()
+
+ADDON = xbmcaddon.Addon('script.reliableresume')
+ADDON_NAME = ADDON.getAddonInfo('name')
+ADDON_ID = ADDON.getAddonInfo('id')
+ADDON_VERSION = ADDON.getAddonInfo('version')
+ADDON_PATH = ADDON.getAddonInfo('path')
+ADDON_USER_PATH = os.path.join(xbmc_translate_path('special://userdata'), 'addon_data', ADDON_ID)
+ADDON_ICON = os.path.join(ADDON_PATH, 'icon.png')
+MONITOR = xbmc.Monitor()
+
+DATAFILE = os.path.join(ADDON_USER_PATH, 'ResumeSaverA.m3u')
+DATAFILE2 = os.path.join(ADDON_USER_PATH, 'ResumeSaverB.m3u')
+
+if PY2:
+    XBMC_LOG_LEVEL = xbmc.LOGNOTICE
+else:
+    XBMC_LOG_LEVEL = xbmc.LOGINFO
+
+
+class ResumeSaver(object):
+    currentFile = 0
+
+    if PY2:
+        lastExecutionTime = time.clock()
+    else:
+        lastExecutionTime = datetime.datetime.now().timestamp()
+    lastConfigReadTime = 0
+
+    timer_amounts = {'0': 5, '1': 30, '2': 120, '3': 300, '4': 600}
+
+    videoEnable = False
+    audioEnable = False
+    executeInterval = 60
+
+    def shouldExecute(self):
+        if PY2:
+            now = time.clock()
+        else:
+            now = datetime.datetime.now().timestamp()
+        if (now - self.lastExecutionTime) >= self.executeInterval:
+            self.lastExecutionTime = now
+            return True
+        return False
+
+    def shouldReadConfig(self):
+        if PY2:
+            now = time.clock()
+        else:
+            now = datetime.datetime.now().timestamp()
+        if (now - self.lastConfigReadTime) >= 5:
+            self.lastConfigReadTime = now
+            return True
+        return False
+
+    def reloadConfigIfNeeded(self):
+        if self.shouldReadConfig():
+            self.videoEnable = get_addon_setting('observe_video')
+            self.videoExcludeLiveTV = get_addon_setting('ExcludeLiveTV')
+            self.videoExcludeHTTP = get_addon_setting('ExcludeHTTP')
+            self.audioEnable = get_addon_setting('observe_audio')
+            self.all_to_music = get_addon_setting('all_to_music')
+            self.executeInterval = self.timer_amounts[get_addon_setting('timer_amount')]
+
+    def loader(self):
+        while not MONITOR.abortRequested():
+            xbmc.sleep(2000)
+
+            self.reloadConfigIfNeeded()
+
+            if not self.shouldExecute():
+                continue
+
+            if not xbmc.Player().isPlaying():
+                continue
+
+            self.playing = xbmc.Player().getPlayingFile()
+            self.playlist = []
+
+            if self.playing.find("pvr://") > -1 and self.videoEnable: 
+                if self.videoExcludeLiveTV:
+                    debug('Video is PVR (Live TV), which is currently set as an excluded source.')
+                    continue
+                else:
+                    self.media = 'pvr'
+                    self.time = xbmc.Player().getTime()
+                    self.plist = xbmc.PlayList(1)
+                    self.plsize = '-'
+                    self.plpos = 0                    
+                    self.writedata()
+            elif xbmc.Player().isPlayingAudio() and self.audioEnable:
+                self.media = 'audio'
+                self.time = xbmc.Player().getTime()
+                self.plist = xbmc.PlayList(0)
+                self.plsize = self.plist.size()
+                self.plpos = self.plist.getposition()
+                if xbmc.Player().getTime() > 0:
+                    self.writedata()            
+            elif xbmc.Player().isPlayingVideo() and self.all_to_music:
+                self.media = 'audio'
+                self.time = xbmc.Player().getTime()
+                self.plist = xbmc.PlayList(0)
+                self.plsize = self.plist.size()
+                self.plpos = self.plist.getposition()
+                if xbmc.Player().getTime() > 0:
+                    self.writedata()
+            elif xbmc.Player().isPlayingVideo() and self.videoEnable:
+                self.media = 'video'
+                self.time = xbmc.Player().getTime()
+                self.plist = xbmc.PlayList(1)
+                self.plsize = self.plist.size()
+                self.plpos = self.plist.getposition()
+                if xbmc.Player().getTime() > 0:
+                    self.writedata()
+            else:
+                continue
+
+            if (self.playing.find("http://") > -1 or self.playing.find("https://") > -1) and self.videoExcludeHTTP:
+                debug("Media is from an HTTP/S source, which is currently set as an excluded source.")
+                continue
+
+    def writedata(self):
+        if self.currentFile == 0:
+            self.writedataex(DATAFILE)
+            self.currentFile = 1
+        else:
+            self.writedataex(DATAFILE2)
+            self.currentFile = 0
+
+    def writedataex(self, datafile):
+        if PY2:
+            f = open(datafile, 'wb')
+        else:
+            f = open(datafile, 'w', encoding=ENCODING)
+        try:
+            f.write('#EXTCPlayListM3U::M3U\n')
+            debug('writing m3u tracks started')
+            if self.plsize != '-':
+                for i in range(0, self.plsize):
+                    if PY2:
+                        debug('#EXTINF:0,{}\n'.format(os.path.split(self.plist[i].getfilename())[1]))
+                        f.write('#EXTINF:0,%s\n' % (os.path.split(self.plist[i].getfilename())[1]))
+                        debug('%s\n' % self.plist[i].getfilename())
+                        f.write('%s\n' % self.plist[i].getfilename())
+                    else:
+                        temp = os.path.split(self.plist[i].getPath())[1]
+
+                        debug('#EXTINF:0,%s\n' % temp)
+                        f.write('#EXTINF:0,%s\n' % temp)
+
+                        temp = self.plist[i].getPath()
+                        debug('%s\n' % temp)
+                        f.write('%s\n' % temp)
+            debug('writing m3u tracks finished')
+
+
+            debug('writing extra tags started')
+
+            debug('#MEDIA::%s\n' % self.media)
+            f.write('#MEDIA::%s\n' % self.media)
+
+            debug('#TIME::%s\n' % self.time)
+            f.write('#TIME::%s\n' % self.time)
+
+            debug('#PLPOS::%s\n' % self.plpos)
+            f.write('#PLPOS::%s\n' % self.plpos)
+
+            debug('#PLSIZE::%s\n' % self.plsize)
+            f.write('#PLSIZE::%s\n' % self.plsize)
+
+            debug('#PLAYING::%s\n' % self.playing)
+            f.write('#PLAYING::%s\n' % self.playing)
+
+            debug('#WINDOW::%s\n' % xbmcgui.getCurrentWindowId())
+            f.write('#WINDOW::%s\n' % xbmcgui.getCurrentWindowId())
+
+            debug('#VOLUME::%s\n' % volume_get())
+            f.write('#VOLUME::%s\n' % volume_get())
+
+            debug('#STORE::DONE\n')
+            f.write('#STORE::DONE\n')
+
+            debug('writing extra tags finished')
+        finally:
+            f.close()
+
+
+def volume_get():
+    request = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Application.GetProperties", "params": { "properties": [ "volume" ] }, "id": 1}')
+    volume = json.loads('%s' % request)['result']['volume']
+    debug('VOLUME: Get %s' % volume)
+    return volume
+
+
+def log(msg):
+    if not PY2:
+        msg = msg.encode("utf-8", "surrogateescape").decode("ISO-8859-1")
+    xbmc.log('%s: SRV: %s' % (ADDON_ID, msg), XBMC_LOG_LEVEL)
+
+
+def note(heading, message=None, time=-1, icon=None):
+    if time == -1:
+        xbmcgui.Dialog().notification(heading='%s' % heading, message='%s' % message if message else ' ', icon='%s' % (icon if icon else ADDON_ICON))
+    else:
+        xbmcgui.Dialog().notification(heading='%s' % heading, message='%s' % message if message else ' ', icon='%s' % (icon if icon else ADDON_ICON), time=time)
+    log('NOTIFICATION: "%s%s"' % (heading, ' - %s' % message if message else ''))
+
+
+def get_addon_setting(id):
+    setting = xbmcaddon.Addon(ADDON_ID).getSetting(id)
+    if setting.lower() == 'true': return True
+    if setting.lower() == 'false': return False
+    return str(setting)
+
+def debug(string):
+    if not get_addon_setting('debug'):
+        return
+    log('DEBUG: %s' % string)
+
+def main():
+    if os.access(ADDON_USER_PATH, os.F_OK) == 0:
+        os.mkdir(ADDON_USER_PATH)
+    if get_addon_setting('autorun'):
+        xbmc.executescript('special://home/addons/script.reliableresume/default.py')
+    m = ResumeSaver()
+    try:
+        m.loader()
+    except RuntimeError:
+        if not 'Unknown addon id' in RuntimeError.message:
+            raise RuntimeError.message
+    del m
+
+
+if __name__ == '__main__':
+    main()
